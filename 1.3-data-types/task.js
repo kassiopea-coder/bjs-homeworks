ffbabaa4bbce4027561190717fadc46e2c7f7ade
Nadezhda Zizevskaya <7_nadezhda@mail.ru>
2021-03-01 19:02:31 +0400

    second commit wrappers-for-primitives

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -2,45 +2,63 @@
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
 
-    if (isNaN(percent)) {
-        return `Параметр 'Процентная ставка' содержит неправильное значение ${percent}`;
-    }
+   //Корректность поля percent
+   if (percent == 0) return "Вы не заполнили поле Процентная ставка";
+   if (percent < 0) return "Процентная ставка не может быть отрицательной";
+   if (isNaN(percent)) {
+      return `Параметр 'Процентная ставка' содержит неправильное значение ${percent}`;
+   };
 
-    if (isNaN(contribution)) {
-        return `Параметр 'Сумма первоначального взноса' содержит неправильное значение ${contribution}`;
-    }
+   //Корректность поля contribution
+   //if (contribution == 0) return "Вы не заполнили поле Начальный взнос";
+   if (contribution < 0) return "Начальный взнос не может быть отрицательный";
+   if (isNaN(contribution)) {
+      return `Параметр 'Сумма первоначального взноса' содержит неправильное значение ${contribution}`;
+   };
 
-    if (isNaN(amount)) {
-        return `Параметр 'Сумма кредита' содержит неправильное значение ${amount}`;
-    }
-    if (isNaN(date)) {
-        return `Параметр 'Дата окончания кредита' содержит неправильное значение ${date}`;
-    }
+   //Корректность поля amount
+   if (amount == 0) return "Вы не заполнили поле Общая стоимость";
+   if (amount < 0) return "Общая стоимость не может быть отрицательной";
+   if (isNaN(amount)) {
+      return `Параметр 'Сумма кредита' содержит неправильное значение ${amount}`;
+   };
+   if (contribution > amount) {
+      return 'Сумма начального взноса не может быть больше общей суммы по кредиту';
+   };
+   //Корректность поля date
+   if (isNaN(date)) {
+      return `Параметр 'Дата окончания кредита' содержит неправильное значение ${date}`;
+   };
+   //const dateNow = new Date();
+   const dateNowYear = new Date().getFullYear();
+   const dateYear = date.getFullYear();
 
-    percent = parseFloat(percent / 100);
-    contribution = parseFloat(contribution);
-    amount = parseFloat(amount);
-    date = new Date(date);
+   let differenceYear = dateYear - dateNowYear;
 
-    let S = amount - contribution;
-    let P = percent / 12;
-    let month = (date.getFullYear() - new Date().getFullYear()) * 12;
+   if (differenceYear < 0) return "Срок кредита не может быть отрицательный";
 
+   percent = parseFloat(percent / 100);
+   contribution = parseFloat(contribution);
+   amount = parseFloat(amount);
+   date = new Date(date);
 
-    let totalAmount = parseFloat(S * (P + P / ((1 + P) ** month - 1)) * month).toFixed(2);
-    console.log(totalAmount);
-    return totalAmount;
+   let S = amount - contribution;
+   let P = percent / 12;
+   let month = differenceYear * 12;
+   let totalAmount = parseFloat((S * (P + P / ((1 + P) ** month - 1)) * month).toFixed(2));
+   console.log(+totalAmount);
+   return +totalAmount;
 
 }
 
 
 function getGreeting(name) {
 
-    if (name === undefined || name.length === 0) {
-        name = 'Аноним';
-    }
+   if (name === undefined || name.length === 0 || (name.indexOf(' ') > -1)) {
+      name = 'Аноним';
+   };
 
-    let greeting = `Привет, мир! Меня зовут ${name}.`;
-    console.log(greeting);
-    return greeting;
+   let greeting = `Привет, мир! Меня зовут ${name}.`;
+   console.log(greeting);
+   return greeting;
 }
