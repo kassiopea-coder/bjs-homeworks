d29b20a36948fc3c5da96455828ccf6de6689cb9
Nadezhda Zizevskaya <7_nadezhda@mail.ru>
2021-03-06 21:51:33 +0400

    first commit object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,46 @@
-//String.prototype.isPalindrome - для задачи №1
+'use strict';
+
+//Задача 1
+
+String.prototype.isPalindrome = function () {
+
+    const string = this.toLowerCase().replace(/\s + /g, '');
+    const reverseString = string.split('').reverse().join('');
+
+    return string === reverseString;
+};
+
+console.log('А роза упала на лапу Азора'.isPalindrome());
+
+//Задача 2
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+
+    if (marks.length === 0) {
+        return 0;
+    }
+
+    let sum = 0;
+
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+    }
+
+    const average = sum / marks.length;
+    const averageMark = Math.round(average);
+
+    return averageMark;
 }
 
+
+//Задача 3
+
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+
+    const now = new Date();
+    const userBirthday = new Date(birthday).getTime();
+    const diff = now - userBirthday;
+    const age = diff / (1000 * 60 * 60 * 24 * 365.25);
+
+    return age >= 18;
 }
